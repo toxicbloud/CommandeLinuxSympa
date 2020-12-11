03365f1bb6b98885803aacfdb122b55ffa21f6e0
Antonin Rousseau <34874048+toxicbloud@users.noreply.github.com>
2020-12-11 19:20:46 +0100

    Update CommandeSympa.sh

--- CommandeSympa.sh
+++ CommandeSympa.sh
@@ -6,6 +6,10 @@ top #gestionnaires des taches en ligne de commande
 htop #gestionnaires des taches en ligne de commande ameliore
 ps #visionner les processus en cours
 uptime #voir depuis combien de temps la machine est allume
+tcpdump #analyse de paquet
+monitor #monitoring
+cron #executer automatiquement des scripts a une heure et date precise ou regulierement
+
 ssh -L 5901:127.0.0.1:80 -N -f pirate@bidouilleur.codes #tunnel ssh qui map le port 5901 du pc executant la console au port 80 du serveur distant bidouilleur.codes
 
 sed '$!d'  #supprime tout ce qui n'est pas la derniere ligne
