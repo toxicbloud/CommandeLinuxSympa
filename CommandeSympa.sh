f593b70d34e0b070ebc8c6c23fa616b805e22d41
Antonin Rousseau <34874048+toxicbloud@users.noreply.github.com>
2020-12-11 19:14:42 +0100

    Update CommandeSympa.sh

--- CommandeSympa.sh
+++ CommandeSympa.sh
@@ -1,3 +1,8 @@
+#commande de base
+sudo apt-get install <packet> <packet 2> #<packet> = nom du paquet : apt-get install discord
+sudo apt-get update #met a jours les listes de paquetss
+sudo apt-get upgrade #met a jour tout les paquets
+
 ssh -L 5901:127.0.0.1:80 -N -f pirate@bidouilleur.codes #tunnel ssh qui map le port 5901 du pc executant la console au port 80 du serveur distant bidouilleur.codes
 
 sed '$!d'  #supprime tout ce qui n'est pas la derniere ligne
