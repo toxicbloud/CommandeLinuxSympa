695c0114e81c6a20515d22a2d6032115b90d906b
Antonin Rousseau <34874048+toxicbloud@users.noreply.github.com>
2020-12-11 18:56:46 +0100

    Create CommandeSympa.sh

---
+++ CommandeSympa.sh
@@ -0,0 +1,3 @@
+ssh -L 5901:127.0.0.1:80 -N -f pirate@bidouilleur.codes #tunnel ssh qui map le port 5901 du pc executant la console au port 80 du serveur distant bidouilleur.codes
+
+sed '$!d'  #supprime tout ce qui n'est pas la derniere ligne
