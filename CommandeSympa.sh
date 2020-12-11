6287bf272e00b7832974a3c0b15242a613116900
Antonin Rousseau <34874048+toxicbloud@users.noreply.github.com>
2020-12-11 19:24:06 +0100

    Update CommandeSympa.sh

--- CommandeSympa.sh
+++ CommandeSympa.sh
@@ -13,12 +13,10 @@ cron #executer automatiquement des scripts a une heure et date precise ou reguli
 vmstat #stat rapide du systeme
 auditd #programmer des alertes si quelqu un ouvre un fichier donne
 lsof #list open files , liste tout les fichiers ouverts
-
 ssh -L 5901:127.0.0.1:80 -N -f pirate@bidouilleur.codes #tunnel ssh qui map le port 5901 du pc executant la console au port 80 du serveur distant bidouilleur.codes
-
 sed '$!d'  #supprime tout ce qui n'est pas la derniere ligne
 
-
+#dangereux
 :(){ :|:& };: #ForkBomb si le nombre de processus est pas limité ça fait mal
 
 #commandes amusantes
