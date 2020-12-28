9b1aae2fd7566e08f770b5b7e9f84dce67d74540
Antonin Rousseau <34874048+toxicbloud@users.noreply.github.com>
2020-12-29 00:40:37 +0100

    Update CommandeSympa.sh

--- CommandeSympa.sh
+++ CommandeSympa.sh
@@ -31,6 +31,9 @@ lsof #list open files , liste tout les fichiers ouverts
 ssh -L 5901:127.0.0.1:80 -N -f pirate@bidouilleur.codes #tunnel ssh qui map le port 5901 du pc executant la console au port 80 du serveur distant bidouilleur.codes
 sed '$!d'  #supprime tout ce qui n'est pas la derniere ligne
 
+#Docker
+docker exec -it <container name> bash #lance un terminal bash dans le container et le relie a ton terminal
+
 #dangereux
 :(){ :|:& };: #ForkBomb si le nombre de processus est pas limité ça fait mal
 
